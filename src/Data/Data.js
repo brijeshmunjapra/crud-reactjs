4a07a9490aee66983614e5b73dfb6acd84480594
Brijesh Munjapra <103625896+brijeshmunjapra@users.noreply.github.com>
2023-04-20 17:44:45 +0530

    Delete src directory

--- src/Data/Data.js
+++
@@ -1,9 +0,0 @@
-export const departments = [
-  { id: 1, name: "Sales" },
-  { id: 2, name: "Finance" },
-  { id: 3, name: "Marketing" },
-  { id: 4, name: "Operations" },
-  { id: 5, name: "Human Resource" },
-  { id: 6, name: "Purchase" },
-  { id: 7, name: "Technical" },
-];
